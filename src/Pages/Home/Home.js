509626567374b218b3eec7bc19599a56682cd3a7
Nikil kumar <kumarnikil220@gmail.com>
2023-01-03 02:59:45 -0800

    storing api data into state

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,16 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./Home.css";
 const Home = () => {
+  // storing data results into state
+  const [popularMovies, setPopularMovies] = useState([]);
   useEffect(() => {
     fetch(
       "https://api.themoviedb.org/3/movie/popular?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US"
     )
       .then((res) => res.json()) //converting got responed to json
       //.then(data=>console.log(data))  //To show data of api
-      .then((data) => console.log(data.results));
+      .then((data) => setPopularMovies(data.results));
   }, []);
-  return <>
-
-  </>;
+  return <></>;
 };
 export default Home;
