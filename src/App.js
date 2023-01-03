990fa60653665378b44f06649276b72eb18d6e6e
Nikil kumar <kumarnikil220@gmail.com>
2023-01-03 01:31:22 -0800

    creating routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import './App.css';
-
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      Hello NIKIL
-      
+      <Router>
+        <Routes>
+          <Route index element={<h1>Hello World</h1>}></Route>
+          <Route path="movie/:id" element={<h1>Movie details page</h1>}></Route>
+          <Route
+            path="movies/:type"
+            element={<h1>Movies list page</h1>}
+          ></Route>
+          <Route path="/*" element={<h1>Error page</h1>}></Route>
+          {/* 
+                 path='/*' ka mean '/' ke baad agar kuch bhi dalega to ye chalega yha 
+                 star yehi show krr rha hai agar / ke baad kuch bhi dala to ye render ho
+                 
+                 path='movie/:id' mean movie/ ke baad kuch bhi daloge  wo isko target karega...
+                 
+                 path='movies/:type'mean movie ke baad jo bhi daloge wo karega isko hit 
+                 */}
+        </Routes>
+      </Router>
     </div>
   );
 }
-
 export default App;
